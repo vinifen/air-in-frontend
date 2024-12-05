4c74e7304ad1797e2c282efad26099758d76ec4a
vinifen <ferreiranivicius@gmail.com>
2024-12-04 21:09:47 -0300

    atv16

--- src/app/pages/home/nav-home/slide-content/sc-search-bar/sc-search-bar.component.ts
+++ src/app/pages/home/nav-home/slide-content/sc-search-bar/sc-search-bar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-sc-search-bar',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, UpperCasePipe],
   templateUrl: './sc-search-bar.component.html',
   styleUrl: './sc-search-bar.component.css'
 })
@@ -14,6 +15,9 @@ export class ScSearchBarComponent {
   formResult: string = 'Result here';
   inputValidation: boolean = false;
   isSubmit: boolean = false;
+  exemplos = [
+    {ola: ['oi bao', "bao oi", "oi oi"]},
+  ]
 
   ngOnInit() {
     const savedValue = sessionStorage.getItem('searchInput');
